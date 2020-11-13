f3998479507808bc5b4a11c2cd67ff0f7e9bf0aa
Angelo <angelo@rcstart.it>
2020-11-13 21:10:18 +0100

    Testing pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -154,55 +154,6 @@ export default {
     '@nuxtjs/dotenv'
   ],
 
-  manifest: {
-    name: process.env.SEO_TITLE ? process.env.SEO_TITLE : `BitSong (BTSG) Blockchain Console`,
-    short_name: process.env.SHORT_TITLE ? process.env.SHORT_TITLE : `BitSong Console`,
-    theme_color: "#424242",
-    description: process.env.SEO_DESCRIPTION ? process.env.SEO_DESCRIPTION : `BitSong Console allows you to interact with the BitSing blockchain in a simple and immediate way. Try the bank, staking, governane, auth and crisis`,
-    background_color: "#F4F4F4",
-    display: "standalone",
-    Scope: "/",
-    start_url: "/",
-    icons: [{
-        src: "/favicon/android-icon-36x36.png",
-        sizes: "36x36",
-        type: "image/png",
-        density: "0.75"
-      },
-      {
-        src: "/favicon/android-icon-48x48.png",
-        sizes: "48x48",
-        type: "image/png",
-        density: "1.0"
-      },
-      {
-        src: "/favicon/android-icon-72x72.png",
-        sizes: "72x72",
-        type: "image/png",
-        density: "1.5"
-      },
-      {
-        src: "/favicon/android-icon-96x96.png",
-        sizes: "96x96",
-        type: "image/png",
-        density: "2.0"
-      },
-      {
-        src: "/favicon/android-icon-144x144.png",
-        sizes: "144x144",
-        type: "image/png",
-        density: "3.0"
-      },
-      {
-        src: "/favicon/android-icon-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-        density: "4.0"
-      }
-    ],
-    splash_pages: null
-  },
-
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/axios
@@ -220,11 +171,17 @@ export default {
     ]
   ],
 
-  // pwa: {
-  //   manifest: {
-
-  //   }
-  // },
+  pwa: {
+    manifest: {
+      name: process.env.SEO_TITLE ? process.env.SEO_TITLE : `BitSong (BTSG) Blockchain Console`,
+      short_name: process.env.SHORT_TITLE ? process.env.SHORT_TITLE : `BitSong Console`,
+      theme_color: "#37474F",
+      description: process.env.SEO_DESCRIPTION ? process.env.SEO_DESCRIPTION : `BitSong Console allows you to interact with the BitSing blockchain in a simple and immediate way. Try the bank, staking, governane, auth and crisis`,
+      background_color: "#F4F4F4",
+      display: "standalone",
+      start_url: "/",
+    }
+  },
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {
